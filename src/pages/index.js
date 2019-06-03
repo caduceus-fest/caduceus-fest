ae7381e3a55be67c6c9ec05a1afcc5ecaf13e4c7
lifecoder <lifecoder.ua@gmail.com>
2019-06-03 22:42:55 +0300

    basic info

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,42 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>International Fest Caduceus</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class="md-grid">
+      <div class="md-cell--12">
+        <h1>International Fest Caduceus</h1>
+      </div>
+      <div class="md-cell--4">
+        <h3>Organizes</h3> 
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+      </div>
+      <div class="md-cell--4">
+        <h3>Genres</h3>
+        <ul>
+          <li>Vocalists</li>
+          <li>Bands</li>
+          <li>Dances (Modern/Folk)</li>
+          <li>Show</li>
+          <li>Majorettes</li>
+          <li>Fashion</li>
+        </ul>
+      </div>
+      <div class="md-cell--4">
+        <h3>Fest Schedule</h3>
+        <ul>
+          <li>10<sup>th</sup> August - Arrival</li>
+          <li>10<sup>th</sup> August - Arrival</li>
+          <li>10<sup>th</sup> August - Arrival</li>
+          <li>10<sup>th</sup> August - Arrival</li>
+          <li>10<sup>th</sup> August - Arrival</li>
+          <li>10<sup>th</sup> August - Arrival</li>
+          <li>10<sup>th</sup> August - Arrival</li>
+          <li>10<sup>th</sup> August - Arrival</li>
+        </ul>
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
