dd471412c0ff0cb0e916a2502ba856ad8f63f5c5
lifecoder <lifecoder.ua@gmail.com>
2019-06-09 20:45:44 +0300

    update site information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Caduceus International Festival`,
+    description: `Join Caduceus festival this Summer!`,
+    author: `@lifecoderua`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
