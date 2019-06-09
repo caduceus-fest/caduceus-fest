a417a712cbadd356042888869f13db1748129f2a
lifecoder <lifecoder.ua@gmail.com>
2019-06-10 00:04:39 +0300

    add hero unit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const organizes = ['alias', 'globus', 'medical', 'publius', 'ub-lawyers'];
@@ -10,20 +9,30 @@ const organizes = ['alias', 'globus', 'medical', 'publius', 'ub-lawyers'];
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div class="md-grid md-grid--no-spacing">
+      <div class="md-cell md-cell--6 nospace">
+        <img src="img/header.jpg" alt="" />
+      </div>
+      <div class="md-cell md-cell--6 hero-unit">
+        <h1 className="md-text-right">Caduceus International Festival</h1>
+        <h2 className="md-text-right">Music</h2>
+        <h2 className="md-text-right">Dance</h2>
+        <h2 className="md-text-right">Fashion</h2>
+      </div>
+    </div>
     <div class="md-grid" style={{backgroundColor:'#4dd0e1', color: 'white'}}>
-      <div class="md-cell--12">
+      <div class="md-cell md-cell--12">
         <h1>International Fest Caduceus</h1>
       </div>
-      <div class="md-cell--4">
+      <div class="md-cell md-cell--4">
         <h3>Organizes</h3> 
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          {/* <Image /> */}
         </div>
         { organizes.map( org => 
-          <img src={`orgs/${org}.jpg`} style={{maxWidth: '50%', maxHeight: '4em'}}></img>
+          <img src={`orgs/${org}.jpg`} style={{maxWidth: '50%', maxHeight: '4em'}} alt="" />
           ) }
       </div>
-      <div class="md-cell--4">
+      <div class="md-cell md-cell--4">
         <h3>Genres</h3>
         <ul>
           <li>Vocalists</li>
@@ -34,7 +43,7 @@ const IndexPage = () => (
           <li>Fashion</li>
         </ul>
       </div>
-      <div class="md-cell--4">
+      <div class="md-cell md-cell--4">
         <h3>Fest Schedule</h3>
         <ul>
           <li>10<sup>th</sup> August - Arrival</li>
