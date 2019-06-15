1f6c4bc3088d07950b5aa87ea778a1191a253a8c
lifecoder <lifecoder.ua@gmail.com>
2019-06-15 23:45:17 +0300

    documents sourcing done

--- src/chunks/documents.jsx
+++ src/chunks/documents.jsx
@@ -1,41 +1,45 @@
 import React from "react"
 import {FontIcon} from "react-md";
+import {StaticQuery} from "gatsby";
 
-const documents = [
-  'doc-123-2456.docx',
-  'doc-123-3456.docx',
-  'doc-123-4456.docx',
-];
-
-const Documents = ( data ) => (
-  <div className="md-grid">
-    { console.log('data', data)}
-    <h3 className='md-cell md-cell--12'>Documents</h3>
-    { documents.map( document =>
-      <a key={document} className='document md-text-center md-cell md-cell--2' href="">
-        <FontIcon>archive</FontIcon>
-        <br/>
-        <span>{document}</span>
-      </a>
+const Documents = () => (
+  <StaticQuery
+    query={graphql`
+      query DocumentsFilesQuery {
+        allFile(filter:{
+          relativeDirectory: {
+            eq: "documents"
+          }
+        }) {
+          edges {
+            node {
+              id
+              name
+              publicURL
+              relativePath
+              prettySize
+              extension
+              birthTime(fromNow: true)
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div className="md-grid">
+        { console.log('data', data)}
+        <h3 className='md-cell md-cell--12'>Documents</h3>
+        { data.allFile.edges.map( ({node}) =>
+          <a key={node.id} className='document md-text-center md-cell md-cell--2' href={node.publicURL}>
+            <FontIcon>archive</FontIcon>
+            <br/>
+            <span>{node.name}</span>
+          </a>
+        )}
+      </div>
     )}
-  </div>
+  />
 );
 
 export default Documents
 
-// Don't work with components, use StaticQuery
-// export const query = graphql`
-//   query {
-//     allFile {
-//       edges {
-//         node {
-//           id
-//           relativePath
-//           prettySize
-//           extension
-//           birthTime(fromNow: true)
-//         }
-//       }
-//     }
-//   }
-// `;
