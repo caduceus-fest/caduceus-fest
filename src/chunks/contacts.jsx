1574f9a104edc7c85ae419f270b91e15fa68c53b
lifecoder <lifecoder.ua@gmail.com>
2019-06-22 13:40:38 +0300

    style contacts section

--- src/chunks/contacts.jsx
+++ src/chunks/contacts.jsx
@@ -1,33 +1,35 @@
 import React from "react"
-import {Button, Chip, FontIcon} from "react-md";
+import {Avatar, Chip, FontIcon} from "react-md";
 
 
 const Contacts = () => (
-  <div className="md-grid">
+  <div className="md-grid hero-unit">
     <h3 className='md-cell md-cell--12'>Contact Us</h3>
     <h4 className='md-cell md-cell--12'>Olga Nadoieva</h4>
 
     <div className='md-cell md-cell--2 md-cell--middle buttons__group'>
-      <div className='md-cell--middle'>
-        <Button icon>phone</Button>
-        <FontIcon>phone</FontIcon>
-        <strong>+80967904265</strong>
-      </div>
-      <p>
-        <Button floating>phone</Button>
-        <span>+80669575802</span>
-      </p>
-
+      <Chip
+        className='spaced-vertical'
+        label="+80967904265"
+        avatar={<Avatar><FontIcon>phone</FontIcon></Avatar>}
+      />
+      <Chip
+        className='spaced-vertical'
+        label="+80669575802"
+        avatar={<Avatar><FontIcon>phone</FontIcon></Avatar>}
+      />
     </div>
     <div className='md-cell md-cell--2 buttons__group'>
-      <p>
-        <Button floating>mail</Button>
-        <span>Olgadruon2017@ukr.net</span>
-      </p>
-      <p>
-        <Button floating>mail_outline</Button>
-        <span>Internationalfestcaduceus@ukr.net</span>
-      </p>
+      <Chip
+        className='spaced-vertical'
+        label="Olgadruon2017@ukr.net"
+        avatar={<Avatar><FontIcon>mail_outline</FontIcon></Avatar>}
+      />
+      <Chip
+        className='spaced-vertical'
+        label="Internationalfestcaduceus@ukr.net"
+        avatar={<Avatar><FontIcon>mail_outline</FontIcon></Avatar>}
+      />
     </div>
   </div>
 );
