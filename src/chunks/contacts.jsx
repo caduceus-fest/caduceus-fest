d16286a7e3a6fe294b261b9d185eb0c0d1706b16
lifecoder <lifecoder.ua@gmail.com>
2019-06-23 13:07:13 +0300

    contacts section links

--- src/chunks/contacts.jsx
+++ src/chunks/contacts.jsx
@@ -8,28 +8,36 @@ const Contacts = () => (
     <h4 className='md-cell md-cell--12'>Olga Nadoieva</h4>
 
     <div className='md-cell md-cell--2 md-cell--middle buttons__group'>
-      <Chip
-        className='spaced-vertical'
-        label="+80967904265"
-        avatar={<Avatar><FontIcon>phone</FontIcon></Avatar>}
-      />
-      <Chip
-        className='spaced-vertical'
-        label="+80669575802"
-        avatar={<Avatar><FontIcon>phone</FontIcon></Avatar>}
-      />
+      <a href="tel:+80967904265" className='no-decoration'>
+        <Chip
+          className='spaced-vertical'
+          label="+80967904265"
+          avatar={<Avatar><FontIcon>phone</FontIcon></Avatar>}
+        />
+      </a>
+      <a href="tel:+80669575802" className='no-decoration'>
+        <Chip
+          className='spaced-vertical'
+          label="+80669575802"
+          avatar={<Avatar><FontIcon>phone</FontIcon></Avatar>}
+        />
+      </a>
     </div>
     <div className='md-cell md-cell--2 buttons__group'>
-      <Chip
-        className='spaced-vertical'
-        label="Olgadruon2017@ukr.net"
-        avatar={<Avatar><FontIcon>mail_outline</FontIcon></Avatar>}
-      />
-      <Chip
-        className='spaced-vertical'
-        label="Internationalfestcaduceus@ukr.net"
-        avatar={<Avatar><FontIcon>mail_outline</FontIcon></Avatar>}
-      />
+      <a href="mailto:Olgadruon2017@ukr.net" className='no-decoration'>
+        <Chip
+          className='spaced-vertical'
+          label="Olgadruon2017@ukr.net"
+          avatar={<Avatar><FontIcon>mail_outline</FontIcon></Avatar>}
+        />
+      </a>
+      <a href="mailto:Internationalfestcaduceus@ukr.net" className='no-decoration'>
+        <Chip
+          className='spaced-vertical'
+          label="Internationalfestcaduceus@ukr.net"
+          avatar={<Avatar><FontIcon>mail_outline</FontIcon></Avatar>}
+        />
+      </a>
     </div>
   </div>
 );
