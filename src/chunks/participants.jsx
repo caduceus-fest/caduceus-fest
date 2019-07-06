800f101db4275fcb4b211e62914c2e5cec4cedbf
lifecoder <lifecoder.ua@gmail.com>
2019-07-06 17:36:25 +0300

    update participants section layout

--- src/chunks/participants.jsx
+++ src/chunks/participants.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 import {StaticQuery} from "gatsby";
-import {Card, Media} from "react-md";
+import Gallery from 'react-grid-gallery';
 
+function formatGalleryImages(fileNodes) {
+  return fileNodes.map(({node}) => ({
+    src: node.publicURL,
+    thumbnail: node.childImageSharp.resize.src,
+    thumbnailWidth: node.childImageSharp.resize.width,
+    thumbnailHeight: node.childImageSharp.resize.height,
+  }))
+}
 
 const Participants = () => (
   <StaticQuery
@@ -21,23 +29,34 @@ const Participants = () => (
               prettySize
               extension
               birthTime(fromNow: true)
+              childImageSharp {
+                original {
+                  width
+                  height
+                }
+                resize(height: 240) {
+                  width
+                  height
+                  src
+                }
+              }
             }
           }
         }
       }
     `}
     render={data => (
-      <div className="md-grid hero-secondary">
+      <div style={{
+        display: "block",
+        minHeight: "1px",
+        width: "100%",
+        overflow: "auto"}}
+        className="hero-secondary">
         <h3 className='md-cell md-cell--12'>Participants</h3>
-        { data.allFile.edges.map( ({ node }) =>
-          <div className="md-cell md-cell--4 jury-member">
-            <Card>
-              <Media>
-                <img src={node.publicURL} alt="" />
-              </Media>
-            </Card>
-          </div>
-        )}
+        <Gallery
+          images={formatGalleryImages(data.allFile.edges)}
+          rowHeight={240}
+          backdropClosesModal={true} />
       </div>
     )}
   />
